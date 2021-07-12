774a93a8c861bddc613b51fea9144e00861f22f8
Özkan İnönlü <ozkaanmax@gmail.com>
2021-07-12 17:53:14 +0300

    Update InstructorManager.java

--- InstructorManager.java
+++ InstructorManager.java
@@ -12,11 +12,14 @@ public class InstructorManager extends UserManager{
 		+ " has been removed from " + course);
 		
 	}
-	public void getInfo(Instructor instructor) {
-		System.out.println("firstName : " +instructor.getFirstName()+
-				"\nlastName : " + instructor.getLastName() + "\ndepartment : "
-				+ instructor.getDepartment() + "\nInstructor ID : " + instructor.getInstructorId());
-		
+	public void addStudentIntoCourse(Student student,String course) {
+		System.out.println(student.getFirstName() + " " + student.getLastName() 
+		+ " has been added into " + course);
 	}
+	public void removeStudentFromCourse(Student student,String course) {
+		System.out.println(student.getFirstName() + " " + student.getLastName() 
+		+ " has been removed from " + course);
+	}
+
 
 }
